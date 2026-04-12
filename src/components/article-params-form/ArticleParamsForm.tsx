1b84148f2c75869b56837e662eea0abb2f171889
vtyurin89 <vlad89main@gmail.com>
2026-04-12 17:17:22 +0300

    feat: Form toggle

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,39 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [formOpened, changeFormState] = useState(false);
+
+	const toggleForm = () => {
+		const prevStateForm = formOpened;
+		changeFormState(!prevStateForm);
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log('SUBMITTING!');
+	};
+
+	const handleReset = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log('RESETING!');
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={formOpened} onClick={toggleForm} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: formOpened,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
