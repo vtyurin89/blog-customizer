a1c47ad0a5fb3d8fb8bb4edf9c9cc833b9e74af2
Vladimir <vlad89main@gmail.com>
2026-04-13 12:22:54 +0300

    feat: Props transfer (css not working yet)

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,6 +1,6 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import { Select } from 'src/ui/select';
 import { RadioGroup } from 'src/ui/radio-group';
@@ -11,11 +11,21 @@ import {
 	backgroundColors,
 	contentWidthArr,
 	fontSizeOptions,
+	ArticleStateType,
 } from 'src/constants/articleProps';
-
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	onApply: (state: ArticleStateType) => void;
+	onReset: () => void;
+	initialValues: ArticleStateType;
+};
+
+export const ArticleParamsForm = ({
+	onApply,
+	onReset,
+	initialValues,
+}: ArticleParamsFormProps) => {
 	const [formOpened, changeFormState] = useState(false);
 
 	const toggleForm = () => {
@@ -23,32 +33,37 @@ export const ArticleParamsForm = () => {
 		changeFormState(!prevStateForm);
 	};
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		// TODO
-		console.log('SUBMITTING!');
-	};
-
-	const handleReset = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		resetFormOptions();
-	};
-
-	const resetFormOptions = () => {
-		setFont(fontFamilyOptions[0]);
-		setFontSize(fontSizeOptions[0]);
-		setFontColor(fontColors[0]);
-		setBackgroundColor(backgroundColors[0]);
-		setContentWidth(contentWidthArr[0]);
-	};
-
-	const [fontSelected, setFont] = useState(fontFamilyOptions[0]);
+	const [fontSelected, setFontFamily] = useState(fontFamilyOptions[0]);
 	const [fontSizeSelected, setFontSize] = useState(fontSizeOptions[0]);
 	const [fontColorSelected, setFontColor] = useState(fontColors[0]);
 	const [backgoundColorSelected, setBackgroundColor] = useState(
 		backgroundColors[0]
 	);
 	const [contentWidthSelected, setContentWidth] = useState(contentWidthArr[0]);
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		onApply({
+			fontFamilyOption: fontSelected,
+			fontSizeOption: fontSizeSelected,
+			fontColor: fontColorSelected,
+			backgroundColor: backgoundColorSelected,
+			contentWidth: contentWidthSelected,
+		});
+	};
+
+	const handleReset = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		onReset();
+	};
+
+	useEffect(() => {
+		setFontFamily(initialValues.fontFamilyOption);
+		setFontSize(initialValues.fontSizeOption);
+		setFontColor(initialValues.fontColor);
+		setBackgroundColor(initialValues.backgroundColor);
+		setContentWidth(initialValues.contentWidth);
+	}, [initialValues]);
 
 	return (
 		<>
@@ -64,7 +79,7 @@ export const ArticleParamsForm = () => {
 					<h2 className={styles.formTitle}>Задайте параметры</h2>
 					<Select
 						selected={fontSelected}
-						onChange={setFont}
+						onChange={setFontFamily}
 						options={fontFamilyOptions}
 						title='Шрифт'
 					/>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,6 +16,17 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const applyChanges = (newState: ArticleStateType) => {
+		setArticleState(newState);
+	};
+
+	const resetChanges = () => {
+		setArticleState(defaultArticleState);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
@@ -25,8 +39,12 @@ const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
-			<Article />
+			<ArticleParamsForm
+				onApply={applyChanges}
+				onReset={resetChanges}
+				initialValues={articleState}
+			/>
+			<Article articleState={articleState} />
 		</main>
 	);
 };
